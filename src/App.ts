5816f1977edd01ead6e2e81c36cb52a45ddb43fe
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-11-05 17:48:09 -0300

    feat: create first comp

---
+++ src/App.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Test = styled.div`
+  position: absolute;
+  bottom: 0;
+  top: 0;
+  background: #000;
+  width: 100%;
+  height: 100%;
+  z-index: 1000;
+`;
