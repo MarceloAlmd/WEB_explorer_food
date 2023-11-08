393f50b263f5c4b02ff54af7bdcc4bc61231e351
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-11-08 15:11:05 -0300

    feat: added favorite

--- src/components/dishes/dishes.comp.tsx
+++ src/components/dishes/dishes.comp.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { DishesProps } from "./dishes";
 import * as Styles from "./dishes.comp.styles";
-import { AiOutlineHeart, AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { AiFillHeart, AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { Button } from "../button/button.comp";
 
 export function Dishes({ title, description, price, ...rest }: DishesProps) {
   const [counter, setCounter] = useState(1);
+  const [isFavorite, setIsFavorite] = useState(false);
 
   function addedMoreCounter() {
     setCounter((prevState) => prevState + 1);
@@ -14,10 +15,38 @@ export function Dishes({ title, description, price, ...rest }: DishesProps) {
   function addedMinusCounter() {
     setCounter((prevState) => prevState - 1);
   }
+
+  function toggleFavorite() {
+    setIsFavorite((prevState) => !prevState);
+  }
+
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+    if (favorites.includes(title)) {
+      setIsFavorite(true);
+    }
+  }, [title]);
+
+  useEffect(() => {
+    if (isFavorite) {
+      const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+      if (!favorites.includes(title)) {
+        favorites.push(title);
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+      }
+    } else {
+      const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+      const index = favorites.indexOf(title);
+      if (index !== -1) {
+        favorites.splice(index, 1);
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+      }
+    }
+  }, [isFavorite, title]);
   return (
     <Styles.Container {...rest}>
-      <Styles.Favorite>
-        <AiOutlineHeart />
+      <Styles.Favorite onClick={toggleFavorite}>
+        <AiFillHeart style={{ color: isFavorite ? "#92000E" : "#7C7C8A" }} />
       </Styles.Favorite>
 
       <Styles.DishImg src="https://img.freepik.com/fotos-gratis/closeup-de-carne-assada-com-molho-legumes-e-batatas-fritas-em-um-prato-sobre-a-mesa_181624-35847.jpg?w=1380&t=st=1699386986~exp=1699387586~hmac=27f97018d94f0be7d20da3f23f6358ba5a2707a708484a4824354cc0dd69d919" />
