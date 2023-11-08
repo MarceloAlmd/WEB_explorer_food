0c439e94f359bb5e38045cd283ccdc62e12578ff
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-11-08 18:12:53 -0300

    styles: category for mobile

--- src/components/category/category.styles.ts
+++ src/components/category/category.styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { BREAK_POINTS } from "../../utils/breakPoints";
 
 export const Container = styled.div`
   width: 100%;
@@ -13,4 +14,12 @@ export const Content = styled.div`
   display: flex;
   gap: 1.688rem;
   overflow-y: hidden;
+
+  @media (max-width: ${BREAK_POINTS.MD}) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    flex-direction: column;
+  }
 `;
